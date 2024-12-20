76bb7f97a8005367170624ba9d3f5833b4b822e5
9090456123 <156017167+9090456123@users.noreply.github.com>
2024-12-20 14:54:04 +0000

    changes have been made

--- job-hunting/backend/controllers/company.controller.js
+++ job-hunting/backend/controllers/company.controller.js
@@ -1,7 +1,90 @@
-export const registerCompany = async (params) => {
+import {Company} from "../models/company.model.js"
+
+
+// registering a company
+
+
+export const registerCompany = async (req,res) => {
     try {
-        
+        const {companyName} = req.body;
+        if(!companyName){
+            return req.status(400).json({
+                message:"Company name is Required",
+                success:false
+            });
+        };
+        let company = await Company.findOne({name:companyName})
+        if(company){
+            return res.status(400).json({
+                message:"You can't register same copmany",
+                success:false
+            })
+        }
+        company = await Company.create({
+            name:companyName,
+            userId:req.id,
+
+        })
+
+        return res.status(201).json({
+            message:"Company is registered sucesssfully",
+            success:true
+        })
     } catch (error) {
+        console.log(error);
+    }
+
+
+
+}
+
+export const getCompany = async (req,res) => {
+    try {
+        const userId = req.id;
+        const companies = await Company.find({userId});
+        if(!companies) {
+            return res.status(404).json({
+                message:"Companies not found",
+                success:false
+            })
+        }
+    } catch (error) {
+        console.log(error);
+        
+    }
+}
+
+
+// getting company by id
+
+export const getCompanyById = async (req,res) => {
+    try {
+        const companyId = req.params.id;
+        const company = await Company.findById(companyId);
+        if(!company){
+            return res.status(404).json({
+                message:"Compsny not found",
+                success:false
+            })
+        }
+        return res.status(200).json({
+            company,
+            success:true
+        })
+    } catch (error) {
+        console.log(error);
+        
+    }
+}
+
+
+// update company
+
+export const updateCompany  = async (req,res) => {
+    try {
+        const {name, description, website, location} = req.body;
+    } catch (error) {
+        console.log(error);
         
     }
 }
